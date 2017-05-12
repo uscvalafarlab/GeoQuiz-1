9919c3899fce57cef765044bb2f49c68e4826d65
DESKTOP-QJKTGG2\Tomahawk <mojomanxero@gmail.com>
2017-05-12 11:35:50 -0400

    Structured comments

--- app/src/main/java/com/example/tomahawk/geoquiz/Question.java
+++ app/src/main/java/com/example/tomahawk/geoquiz/Question.java
@@ -6,6 +6,7 @@ package com.example.tomahawk.geoquiz;
 
 public class Question { // Question objects
 
+    private boolean mAnswered;
     private int mTextResId; // Member variable int
     private boolean mAnswerTrue; // Member variable bool
 
@@ -17,16 +18,22 @@ public class Question { // Question objects
         mTextResId = textResId;
     }
 
-    public boolean isAnswerTrue() { // Getter/Accessor
-        return mAnswerTrue;
-    }
+    public boolean isAnswerTrue() { return mAnswerTrue; } // Getter/Accessor
 
     public void setAnswerTrue(boolean answerTrue) { // Setter/Mutator
         mAnswerTrue = answerTrue;
     }
 
+    public boolean getAnswered() { return mAnswered; }
+
+    public void setAnswered(boolean answered){ mAnswered = answered; }
+
+
     public Question(int textResId, boolean answerTrue){ // Constructor of Question object
         mTextResId = textResId; //Is an int, holds the id for the string resource
         mAnswerTrue = answerTrue; //I think this is self explanatory, and, it holds a boolean thus completing the question object. For now.
+        mAnswered = false; // Added variable to keep track if questions have been answered or not
     }
+
+
 }
--- app/src/main/java/com/example/tomahawk/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/tomahawk/geoquiz/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.example.tomahawk.geoquiz;
 
+import android.icu.text.DecimalFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +15,7 @@ public class QuizActivity extends AppCompatActivity {
 
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
+    private static final String QUESTION_LIST = "question_list";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -22,20 +24,41 @@ public class QuizActivity extends AppCompatActivity {
     private TextView mQuestionTextView;
 
     private int mCurrentIndex = 0;
-    private int TokenArray[] = {7, 7, 7, 7, 7, 7}; // 7 used as default value
-    //private int i = 0;
+    private double Correct = 0;
+    private int TotalAnswered = 0;
+    private double Score = 0;
+
 
     /**
-     * To disable the buttons to complete the challenge...
+     * Getting things set up and ready to be inspected.
+     * Green code ares like this will be the challenges commented out, except for this first block
+     * Grey code will be comments on the methods and code itself about how things work
+     * Log statements to follow how everything works and for debugging purposes
+     * There were some bugs that the BNR did not discuss in any way,
+     * like hitting the back button once reaching the first index would crash the app.
+     */
+
+    /**
+     * Challenge: Disable a question after it has been answered
      *
+     * To disable the buttons to complete the challenge...
      * Add extra bool variable to the question object.
      * Mark if question was answered.---> requires some logic and algorithms
      * Store marked questions in array or hash map to cross check if answered
-     * Create methods to check if question at given index has been answered and if so disable button, if not enable button
+     * Create methods to check if question at given index has been answered and if so disable button, if not enable button.
      *
+     * Challenge complete
      */
 
-    private Question[] mQuestionBank = new Question[] {
+    /**
+     * Challenge: Display game score toast once all questions have been answered
+     * Create a couple variables and keep track of them in the button presses
+     * Display score once all questions have been answered
+     *
+     * Challenge complete, very easy...
+     */
+
+    private Question[] mQuestionBank = new Question[] { // An array of question objects
             new Question(R.string.question_australia, true),
             new Question(R.string.question_oceans, true),
             new Question(R.string.question_mideast, false),
@@ -54,14 +77,22 @@ public class QuizActivity extends AppCompatActivity {
             Log.d(TAG, "Checking savedInstanceState 1");
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
             Log.d(TAG, "Checking savedInstanceState 2");
+            /**
+            * // This is where the saved answers get re-initialized up onCreate() being called after an onDestroy() being called
+            boolean[] mQuestionAnswerArray = savedInstanceState.getBooleanArray(QUESTION_LIST);
+            //assert mQuestionAnswerArray != null;
+            for(int i = 0; i < mQuestionBank.length; i++){
+                mQuestionBank[i].setAnswered(mQuestionAnswerArray[i]);
+            }
+            */
         }
 
         Log.d(TAG, "QuestionTextView");
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         // This is a challenge that did not get commented out because it's hidden and kinda fun to use
         mQuestionTextView.setOnClickListener(new View.OnClickListener(){ // Set an onClickListener to the TextView
-            public void onClick(View v){                                 // thereby allowing the user to change questions by clicking the text
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+            public void onClick(View v){ // Allows the user to change questions by clicking the text
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length; // Calculates index
                 int question = mQuestionBank[mCurrentIndex].getTextResId();
                 mQuestionTextView.setText(question);
             }
@@ -74,23 +105,8 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                int i = 0;
                 checkAnswer(true); // Calling the checkAnswer method to check the answer
-                //isCheckedTrue(); // Dunno if I need this or not, bout to find out
-                do {
-                    mTrueButton.setEnabled(true);
-
-                }
-                while(mCurrentIndex != TokenArray[i]);
-
-                if(i == 6){
-                    i = 0;
-                }else{
-                    i++;
-                }
-
                 //Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show(); // This is how we make a toast, one way at least
-
             }
         });
 
@@ -99,8 +115,7 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Toast.makeText(QuizActivity.this, R.string.wrong_toast, Toast.LENGTH_SHORT).show();
-                isCheckedFalse();
+                //Toast.makeText(QuizActivity.this, R.string.wrong_toast, Toast.LENGTH_SHORT).show()
                 checkAnswer(false); // Same as above
             }
         });
@@ -109,9 +124,13 @@ public class QuizActivity extends AppCompatActivity {
         mNextButton = (Button) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener(){ // OnClickListener
             public void onClick(View v){
+                Log.d(TAG, "Next Button listener");
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length; // Calculates question index
                 int question = mQuestionBank[mCurrentIndex].getTextResId(); // Uses index to extract res id and sets to question
                 mQuestionTextView.setText(question); // Uses question (the text res id), to set text to the textView
+                /**
+                setButtons();
+                */
             }
         });
 
@@ -121,6 +140,7 @@ public class QuizActivity extends AppCompatActivity {
         mBackButton = (Button) findViewById(R.id.back_button);
         mBackButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
+                Log.d(TAG, "Back Button listener");
                 if(mCurrentIndex == 0){
                     mCurrentIndex = 0;
                 } else {
@@ -128,54 +148,78 @@ public class QuizActivity extends AppCompatActivity {
                     int question = mQuestionBank[mCurrentIndex].getTextResId();
                     mQuestionTextView.setText(question);
                 }
+                /**
+                setButtons();
+                */
             }
         });
 
         updateQuestion(); // Call to the updateQuestion method
-
     }
 
-
-    private void isCheckedTrue(){
-        for(int i = 0; i < TokenArray.length; i++){
-            if(mCurrentIndex == TokenArray[i]){
-               mTrueButton.setEnabled(false);
-            }else{
-                TokenArray[i] = mCurrentIndex;
-                mTrueButton.setEnabled(true);
-            }
+    /**
+    // This method sets the buttons to enabled or disabled according to the question being answered or not
+    // Part of the a challenge
+    private void setButtons() {
+        Log.d(TAG, "1st setButtons called");
+       if (mQuestionBank[mCurrentIndex].getAnswered()){ // Simple check to see if the question at a particular index has been answered
+           mTrueButton.setEnabled(false); // If yes it disables the buttons
+           mFalseButton.setEnabled(false);
+       }else{
+           Log.d(TAG, "2nd setButtons called");
+           mTrueButton.setEnabled(true); // Otherwise it enables the buttons
+           mFalseButton.setEnabled(true);
         }
     }
-
-    private void isCheckedFalse(){
-        for(int i = 0; i < TokenArray.length; i++){
-            if(mCurrentIndex == TokenArray[i]){
-                mFalseButton.setEnabled(false);
-            }else{
-                TokenArray[i] = mCurrentIndex;
-                mFalseButton.setEnabled(true);
-            }
-        }
-    }
+    */
 
     private void updateQuestion(){ // Method that gets a question based on index and sets it to the TextView
         Log.d(TAG, "updateQuestion() called");
-        int question = mQuestionBank[mCurrentIndex].getTextResId();
-        mQuestionTextView.setText(question);
+        int question = mQuestionBank[mCurrentIndex].getTextResId(); // Gets the Resource Id for the question at current index
+        mQuestionTextView.setText(question); // Sets the question in the text view
+        /**
+        setButtons();
+        */
     }
 
-   private void checkAnswer(boolean userPressedTrue){ // Method that checks if the user got the answer right or wrong
+    // Method that checks if the user got the answer right or wrong
+   private void checkAnswer(boolean userPressedTrue){
        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue(); // Retrieves the boolean from the question and sets it to answerIsTrue
 
        int messageResId = 0;
 
        if(userPressedTrue == answerIsTrue){ // Logic to check if user answered correctly
-           messageResId = R.string.correct_toast;
+           /**
+           mQuestionBank[mCurrentIndex].setAnswered(true);
+           Correct += 1;
+           TotalAnswered += 1;
+           */
+            messageResId = R.string.correct_toast;
        } else {
-           messageResId = R.string.wrong_toast;
+           /**
+           mQuestionBank[mCurrentIndex].setAnswered(true);
+           TotalAnswered += 1;
+           */
+            messageResId = R.string.wrong_toast;
        }
-       Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+       /**
+       setButtons();
+       checkScore();
+       */
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
    }
+
+    /**
+    // Challenge: This method checks if all the questions have been answered and if so calculates the score and displays a toast
+    public void checkScore(){ // It can be modified to reset the quiz to be retaken
+        if (TotalAnswered == mQuestionBank.length){
+            Score = (Correct / mQuestionBank.length) * 100;
+            String str = String.format("%.2f", Score);
+            //R.string.quiz_score + Score
+            Toast.makeText(this, "Your score is: " + str , Toast.LENGTH_LONG).show(); //Displays the players score, Challenge
+        }
+    }
+    */
 
     // Overriding these methods to learn logging and stack tracing
     @Override
@@ -201,6 +245,16 @@ public class QuizActivity extends AppCompatActivity {
         super.onSaveInstanceState(savedInstanceState);
         savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
         Log.d(TAG, "onSaveInstanceState");
+
+        /**
+        // To save the answered questions upon onDestroy() method being called
+        boolean[] mQuestionAnswerArray = new boolean[mQuestionBank.length];
+        for(int i = 0; i < mQuestionBank.length; i++){
+            mQuestionAnswerArray[i] = mQuestionBank[i].getAnswered();
+            Log.d(TAG, " inside for loop onSaveInstanceState");
+        }
+        savedInstanceState.putBooleanArray(QUESTION_LIST, mQuestionAnswerArray);
+        */
     }
 
     @Override
